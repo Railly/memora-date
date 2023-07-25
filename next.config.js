2d0f4e51a17f1934e381eed5de198fbdabb13aac
Carlos Tarmeno <carlos-100-800@hotmail.com>
2023-07-25 17:29:45 -0500

    feat: redirect root to sign-in

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/sign-in",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
