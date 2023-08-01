f380f85533e5a5a715dc6b757ddb71a2dc2459f6
Carlos Tarmeno <carlos-100-800@hotmail.com>
2023-07-31 23:31:39 -0500

    feat: add PWA support icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,6 +78,29 @@ export const metadata: Metadata = {
     images: [siteConfig.ogImage],
     creator: "@raillyhugo",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png" },
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
