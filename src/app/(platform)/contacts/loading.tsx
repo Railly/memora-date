7863915f9e5f8ed749e50502ae8127a0f9a8c773
EdwardR0507 <ramosedward21@gmail.com>
2023-08-25 20:18:15 -0500

    fix: details cards and loading pages

--- src/app/(platform)/contacts/loading.tsx
+++ src/app/(platform)/contacts/loading.tsx
@@ -1,9 +1,16 @@
 import { ContactsSection } from "@/components/contacts/contacts-section";
+import { SubHeader } from "@/components/shared/molecules/sub-header";
+import { Badge } from "@/components/ui/badge";
 
 export default function ContactLoading() {
   return (
     <div className="flex justify-center w-full">
       <div className="flex flex-col items-center w-full gap-6">
+        <SubHeader title="My Contacts">
+          <Badge variant="yellow">
+            <span className="animate-pulse">0 contacts</span>
+          </Badge>
+        </SubHeader>
         <ContactsSection contacts={null} user={null} isSkeleton />
       </div>
     </div>
