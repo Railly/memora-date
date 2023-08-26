33e7caea435c5fce7de24dfacc9d594258fcab35
Carlos Tarmeno <carlos-100-800@hotmail.com>
2023-08-26 01:29:42 -0500

    fix: dynamic height in auth pages

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,7 +3,7 @@ import AuthBanner from "@/components/forms/auth/auth-banner";
 
 export default async function SignUpPage() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen dark:bg-black">
+    <div className="flex items-center justify-center w-screen h-[100dvh] dark:bg-black">
       <div className="flex flex-col items-center mx-4 w-[20.625rem]">
         <AuthBanner title="Create your Account" />
         <SignUpForm />
