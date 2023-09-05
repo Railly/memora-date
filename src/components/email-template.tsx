67e4fa01eec13f18fd1da62e975dcbbd0151d822
Railly <railly_2009@hotmail.com>
2023-09-04 22:30:24 -0500

    feat: add email template for reminders

--- src/components/email-template.tsx
+++ src/components/email-template.tsx
@@ -2,8 +2,16 @@ import { Tailwind } from "@react-email/tailwind";
 import { Heading } from "@react-email/heading";
 
 interface EmailTemplateProps {
-  event: any;
-  reminder: any;
+  event: {
+    name: string;
+    description: string;
+  };
+  reminder: {
+    reminder_type: string;
+    time: string;
+    date: Date;
+    notificationMethod: string;
+  };
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
@@ -11,11 +19,31 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   reminder,
 }) => (
   <Tailwind>
-    <Heading className="px-6 py-4 text-black bg-rose-500" as="h2">
-      {event.name}
+    <Heading className="px-6 py-4 text-black" as="h2">
+      {`${event.name}: A Personal Reminder from Memora Date`}
     </Heading>
     <div className="px-6 py-4 text-gray-700 bg-gray-100">
-      <p>{event.description}</p>
+      <p>Hey there,</p>
+      <p>
+        You set a reminder for {event.name}, and it&apos;s time to get ready.
+      </p>
+      <p>
+        👉 What&apos;s This About?: {event.description}
+        <br />
+        🕒 Reminder Time: {reminder.time}
+        <br />
+        📆 Reminder Date: {reminder.date.toDateString()}
+        <br />
+        🔔 Type of Reminder: {reminder.reminder_type}
+      </p>
+      <p>
+        Remember, time waits for no one. Take action and make the most of your
+        day.
+      </p>
+      <p>
+        Best, <br />
+        Memora Date
+      </p>
     </div>
   </Tailwind>
 );
