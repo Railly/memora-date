f9165f3da0baf4f3472656469ffb135ca9619b07
Railly Hugo <51397083+Railly@users.noreply.github.com>
2023-09-04 21:35:19 -0500

    fix: handle callback for recurring events (#33)
    * feat: first PoC qstash redis
    
    * fix: add correct notBefore value
    
    * feat: integrate qstash with reminders
    
    * fix: add coalescing operator to routing utils
    
    * fix: add next event algorithm
    
    * fix: handle callback for recurring events

--- src/app/api/reminders/[id]/route.ts
+++ src/app/api/reminders/[id]/route.ts
@@ -6,15 +6,61 @@ export async function POST(_: Request, { params }: { params: { id: string } }) {
   try {
     const serverApiProvider = new ServerApiProvider({ cookies });
 
-    const response =
-      await serverApiProvider.reminder.deleteRecurrentReminderSchedule({
-        scheduleId: params.id,
-      });
+    const rawReminder = await serverApiProvider.reminder.getReminder({
+      reminder_id: params.id,
+    });
 
-    if (response.error) {
-      return ApiResponse.serverError(response.error.message, response.error);
+    const currentReminder = rawReminder.data?.[0];
+
+    if (!currentReminder) {
+      return ApiResponse.serverError("Reminder not found", rawReminder.error);
     }
-    return ApiResponse.success(response.data);
+
+    if (currentReminder.recurrence_type === "After") {
+      if (
+        Number(currentReminder?.sent_count) >=
+        Number(currentReminder?.recurrence_value)
+      ) {
+        const response =
+          await serverApiProvider.reminder.deleteRecurrentReminderSchedule({
+            scheduleId: params.id,
+          });
+
+        if (response.error) {
+          return ApiResponse.serverError(
+            response.error.message,
+            response.error
+          );
+        }
+        return ApiResponse.success(response.data);
+      }
+    }
+
+    if (currentReminder.recurrence_type === "Until") {
+      if (!currentReminder?.recurrence_value) {
+        return ApiResponse.serverError(
+          "Something went wrong, please try again later",
+          rawReminder.error
+        );
+      }
+      if (
+        new Date(currentReminder?.recurrence_value).getTime() <=
+        new Date().getTime()
+      ) {
+        const response =
+          await serverApiProvider.reminder.deleteRecurrentReminderSchedule({
+            scheduleId: params.id,
+          });
+
+        if (response.error) {
+          return ApiResponse.serverError(
+            response.error.message,
+            response.error
+          );
+        }
+        return ApiResponse.success(response.data);
+      }
+    }
   } catch (error) {
     return ApiResponse.serverError(
       "Something went wrong, please try again later",
